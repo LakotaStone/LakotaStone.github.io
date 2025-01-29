5dfac4a01109d4e07abc2a3578ec6bf8739a6db9
LakotaStone <Lakotamackworks@gmail.com>
2025-01-29 11:18:03 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 <script src="script.js"></script>
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("clickMe").addEventListener("click", function() {
+        alert("Thanks for clicking!");
+    });
+});
